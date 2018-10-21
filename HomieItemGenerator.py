406c4843e470bc284f49c7da709a2d3a7c88175c
reapzor <reapzor@gmail.com>
2018-10-20 23:07:55 -0400

    Begin defining item file generator logic

--- HomieItemGenerator.py
+++ HomieItemGenerator.py
@@ -3,8 +3,8 @@ HOMIE_PROPERTIES = [
     {"property_name": "name",                "property_path": "$name",                            "value_type": "String",        "display_text": "Device Name [%s]",                 "icon": "keyring",              "transform": "default"},
     {"property_name": "mac",                 "property_path": "$mac",                             "value_type": "String",        "display_text": "MAC [%s]",                         "icon": "text",                 "transform": "default"},
     {"property_name": "ip",                  "property_path": "$localip",                         "value_type": "String",        "display_text": "IP [%s]",                          "icon": "text",                 "transform": "default"},
-    {"property_name": "signal",              "property_path": "$signal",                          "value_type": "Number",        "display_text": "Signal [%.1f%%]",                  "icon": "network",              "transform": "default"},
-    {"property_name": "uptime",              "property_path": "$uptime",                          "value_type": "Number",        "display_text": "Uptime [JS(uptime.js):%s]",        "icon": "time",                 "transform": "default"},
+    {"property_name": "signal",              "property_path": "$stats/signal",                    "value_type": "Number",        "display_text": "Signal [%.1f%%]",                  "icon": "network",              "transform": "default"},
+    {"property_name": "uptime",              "property_path": "$stats/uptime",                    "value_type": "Number",        "display_text": "Uptime [JS(uptime.js):%s]",        "icon": "time",                 "transform": "default"},
     {"property_name": "fw_name",             "property_path": "$fw/name",                         "value_type": "String",        "display_text": "FW Name [%s]",                     "icon": "text",                 "transform": "default"},
     {"property_name": "fw_version",          "property_path": "$fw/version",                      "value_type": "String",        "display_text": "FW Version [%s]",                  "icon": "text",                 "transform": "default"},
     {"property_name": "current_config",      "property_path": "$implementation/config",           "value_type": "String",        "display_text": "Config JSON [%s]",                 "icon": "settings",             "transform": "default"},
@@ -27,17 +27,17 @@ def build_name(*elements):
     return "_".join(elements)
 
 
-class HomieishProperty:
+class HomieHabProperty:
 
     def __init__(self, property_name, root_name, property_path, root_path, value_type,
-                 group_name, display_text, icon, transform="default", settable=False, readable=True):
+                 owner_group_name, display_text, icon="none", transform="default", settable=False, readable=True):
         self.property_name = property_name
         self.root_name = root_path
         self.name = build_name(root_name, property_name)
         self.property_path = property_path
         self.root_path = root_path
         self.value_type = value_type
-        self.group_name = group_name
+        self.owner_group_name = owner_group_name
         self.display_text = display_text
         self.icon = icon
         self.transform = transform
@@ -46,7 +46,7 @@ class HomieishProperty:
         self.path = build_mqtt_path(root_path, property_path)
 
 
-class HomieishNode:
+class HomieHabNode:
 
     def __init__(self, node_name, root_name, node_path, root_path, node_group_name):
         self.node_name = node_name
@@ -54,14 +54,14 @@ class HomieishNode:
         self.name = build_name(root_name, node_name)
         self.node_path = node_path
         self.root_path = root_path
-        self.group_name = node_group_name
-        self.group_name = build_name(node_group_name, node_name, "Node")
+        self.node_group_name = node_group_name
+        self.group_name = build_name(node_group_name, node_name)
         self.path = build_mqtt_path(root_path, node_path)
         self.node_properties = []
 
     def add_property(self, **kwargs):
-        homie_property = HomieishProperty(
-            **kwargs, root_name=self.name, root_path=self.path, group_name=self.group_name)
+        homie_property = HomieHabProperty(
+            **kwargs, root_name=self.name, root_path=self.path, owner_group_name=self.group_name)
         self.node_properties.append(homie_property)
         return homie_property
 
@@ -76,12 +76,14 @@ class HomieishNode:
                 return homie_property
 
 
-class HomieishItem:
+class HomieHabItem:
 
-    def __init__(self, name, root_path, device_id):
-        self.name = name
-        self.group_name = "g" + name
-        self.settings_group_name = build_name(self.group_name, "Item", "Properties")
+    def __init__(self, item_name, location_name, root_path, device_id):
+        self.item_name = item_name
+        self.location_name = location_name
+        self.name = build_name(location_name, item_name)
+        self.group_name = build_name("g", location_name, item_name)
+        self.settings_group_name = build_name(self.group_name, "Properties")
         self.base_path = root_path
         self.device_id = device_id
         self.path = build_mqtt_path(root_path, device_id)
@@ -91,11 +93,11 @@ class HomieishItem:
     def _generate_device_properties(self):
         self.item_properties = []
         for property_dict in HOMIE_PROPERTIES.copy():
-            self.item_properties.append(HomieishProperty(
-                **property_dict, root_name=self.name, root_path=self.path, group_name=self.settings_group_name))
+            self.item_properties.append(HomieHabProperty(
+                **property_dict, root_name=self.name, root_path=self.path, owner_group_name=self.settings_group_name))
 
     def add_node(self, **kwargs):
-        homie_node = HomieishNode(
+        homie_node = HomieHabNode(
             **kwargs, root_name=self.name, root_path=self.path, node_group_name=self.group_name)
         self.item_nodes.append(homie_node)
         return homie_node
@@ -111,29 +113,69 @@ class HomieishItem:
                 return item_property
 
 
-class HomieOpenhabItemGenerator:
+class HomieHabItemGenerator:
 
     def __init__(self, broker_name):
         self.broker_name = broker_name
         self.homie_items = []
+        self.generated_homie_items = None
 
     def add_item(self, **kwargs):
-        homie_item = HomieishItem(**kwargs)
+        homie_item = HomieHabItem(**kwargs)
         self.homie_items.append(homie_item)
         return homie_item
 
     def homie_item(self, name):
         for homie_item in self.homie_items:
-            if homie_item.name == name:
+            if homie_item.item_name == name:
                 return homie_item
 
+    def generate_items(self):
+        self.generated_homie_items = []
+        for homie_item in self.homie_items:
+            item_groups = self.generate_item_groups(homie_item)
+            property_item_map = self.generate_property_item_map(homie_item)
+            node_item_map = self.generate_node_item_map(homie_item)
+            self.generated_homie_items.append(
+                {"item_groups": item_groups, "property_item_map": property_item_map, "node_item_map": node_item_map})
+        return self.generated_homie_items
 
-item = HomieishItem("CoolItem", "Test1/Test2/", "me")
+    def generate_item_groups(self, homie_item):
+        groups = list()
+        groups.append(self._build_item_group(homie_item.group_name))
+        groups.append(self._build_item_group(homie_item.settings_group_name))
+        for node in homie_item.item_nodes:
+            groups.append(self._build_item_group(node.group_name))
+        return groups
+
+    def generate_property_item_map(self, homie_item):
+        items = []
+        for item_property in homie_item.item_properties:
+            items.append(self._build_item(self.broker_name, item_property))
+        return items
+
+    def generate_node_item_map(self, homie_item):
+        items = dict()
+        return items
+
+    def save_item_files(self):
+        pass
+
+    def _build_item_group(self, name):
+        return "Group " + name
+
+    def _build_item(self, broker_name, item_property):
+        return "hi"
+
+
+generator = HomieHabItemGenerator("broker.me")
+item = generator.add_item(item_name="CoolItem", location_name="CoolLocation", root_path="Test1/Test2/", device_id="me")
 item.add_node(node_name="nodeName", node_path="nodePath").add_properties(
     {"property_name": "newProperty", "property_path": "$propertyPath", "value_type": "StringType", "display_text": "testTest", "icon": "temperatore"},
-    {"property_name": "newPropertyTwo", "property_path": "propertyPathTwo", "value_type": "StringTypeTwo", "display_text": "testTest", "icon": "temperatore"},
+    {"property_name": "newPropertyTwo", "property_path": "propertyPathTwo", "value_type": "StringTypeTwo", "display_text": "testTest"},
 )
 
-print(item.item_node("nodeName").node_property("newProperty").icon)
-print(item.item_property("signal").name)
+print(item.item_node("nodeName").node_property("newProperty").path)
+print(item.item_property("signal").owner_group_name)
 
+print(generator.generate_items()[0])
